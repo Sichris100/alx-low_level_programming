61abb751833e7975b846f8d123402ed214703561
Sichris100 <sichris100@gmail.com>
2022-09-11 17:32:11 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h
+#include <stdio.h>
 
 /**
 * main - print the string in the put function
 *
-*Description: using the main function
-*this program prints "programming is like building a multiligual puzzle
-*return: 0
+* Description: using the main function
+* this program prints "programming is like building a multiligual puzzle
+* Return: 0
 */
 int main(void)
 {
